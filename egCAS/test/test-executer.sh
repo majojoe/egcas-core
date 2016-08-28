ffc0fdf4a0e782b864013f57fc873015592926f4
majojoe <maier_jo@gmx.de>
2016-08-28 18:02:05 +0200

    :internal: test executer works now

--- egCAS/test/test-executer.sh
+++ egCAS/test/test-executer.sh
@@ -1,14 +1,30 @@
 #!/bin/bash -e
 
 
-
 if [ -z $1 ]; then
+        echo "no target directory given"
+        echo "exiting..."
+        exit 2
+fi
+
+if [ -z $2 ]; then
         echo "no arguments given"
         echo "exiting..."
+        exit 1
 fi
+
 
 #iterate over all arguments of the script
 for arg in "$@"
 do
-        echo $PWD "$arg"
+        FILE_TO_COPY=$(find . -name "$arg")
+        if [ -n "${FILE_TO_COPY}" ]; then
+                "${FILE_TO_COPY}"
+        fi
+        
 done
+
+
+echo -e "\e[32m==============================================\e[0m"
+echo -e "\e[32m         all tests executed successful\e[0m"
+echo -e "\e[32m==============================================\e[0m"
