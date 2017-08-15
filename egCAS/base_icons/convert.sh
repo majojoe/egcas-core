8d66dee7e19fb67c6f6b7e47c0b7ceb98c1a1651
majojoe <maier_jo@gmx.de>
2017-08-15 05:53:08 +0200

    :internal: add icons

--- egCAS/base_icons/convert.sh
+++ egCAS/base_icons/convert.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 RESSORCE_DIR="../src/res/"
 convert -density 200 calculator.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/calculator.png" 
+convert -density 200 egcas.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/egcas.png" 
