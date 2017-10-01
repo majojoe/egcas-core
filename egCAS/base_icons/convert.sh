4a046314efc45d178ce574221f8188e3ea57165b
majojoe <maier_jo@gmx.de>
2017-10-01 19:48:37 +0200

    :internal: add some icons

--- egCAS/base_icons/convert.sh
+++ egCAS/base_icons/convert.sh
@@ -5,5 +5,8 @@ convert -density 200 egcas.svg -resize 32x32 -transparent white "${RESSORCE_DIR}
 convert -density 200 insert_graphic.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/insert_graphic.png"
 convert -density 200 insert_text.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/insert_text.png"
 convert -density 200 egcas.svg -resize 2560x2560 -transparent white "${RESSORCE_DIR}/egcas_tmp.png" 
+convert -density 200 italic_text.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/italic_text.png"
+convert -density 200 underlined_text.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/underlined_text.png"
+convert -density 200 bold_text.svg -resize 32x32 -transparent white "${RESSORCE_DIR}/bold_text.png"
 convert "${RESSORCE_DIR}/egcas_tmp.png"  -bordercolor white -border 0 \( -clone 0 -resize 16x16 \) \( -clone 0 -resize 32x32 \) \( -clone 0 -resize 48x48 \) \( -clone 0 -resize 64x64 \) \( -clone 0 -resize 128x128 \) -delete 0 -alpha off -colors 256 "${RESSORCE_DIR}/egcas.ico"
 rm "${RESSORCE_DIR}/egcas_tmp.png"
