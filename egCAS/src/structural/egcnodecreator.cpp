b67a2c0f5eff51041894112b0435c1945330df08
majojoe <maier_jo@gmx.de>
2018-07-20 21:04:47 +0200

    :internal: refactor cog scripts (now in external script)

--- egCAS/src/structural/egcnodecreator.cpp
+++ egCAS/src/structural/egcnodecreator.cpp
@@ -38,50 +38,12 @@ EgcNode* EgcNodeCreator::create(EgcNodeType type)
         import cog
         import os
         import re
+        import sys
+        sys.path.append(BaseDirToSearch + "/../tools/")
+        import python_helper as hlp
 
-        def find_nodes_in_file(filename): 
-            nodes = list()
-            regex = re.compile("[\s]*EGC_SET_EXPRESSION_TYPE\(([a-zA-Z0-9_-]+)[\s]*,[\s]*[a-zA-Z0-9]+::([a-zA-Z0-9]+)[\s]*\)[\s]*;[\s]*", re.MULTILINE)
-            with open(filename, "r") as f:
-                content = f.read();
-                result = regex.findall(content)
-                if result:
-                    nodes.append(result)
-                else:
-                    nodes = None
-                f.close()
-                return nodes
-            return None
-
-        def find_nodes_in_dir(base_path, sub_path):
-            nodes = list()
-            dir_complete = os.path.join(base_path, sub_path)
-            files = list()
-            for root, directories, filenames in os.walk(dir_complete):
-                for filename in filenames: 
-                    files.append(os.path.join(root,filename))
-            for file in files:
-                if file.endswith(".h") or file.endswith(".H"):
-                    nodes_temp = find_nodes_in_file(file)
-                    if nodes_temp:
-                        nodes.append(nodes_temp)
-            if not nodes:
-                cog.error("Seems you have changed the macro name \"EGC_SET_EXPRESSION_TYPE\" please change the above regex also to function again!")
-            return nodes
-
-        def flatten(param):
-            if not param: 
-                return []
-            elif type(param) is not list: 
-                return [param]
-            else:
-                first_el = flatten(param[0])
-                el = flatten(param[1:])
-                first_el.extend(el)
-                return first_el
-            
-        nodes = find_nodes_in_dir(BaseDirToSearch, "structural")
-        nodes = flatten(nodes)
+        nodes = hlp.find_nodes_in_dir(BaseDirToSearch, "structural")
+        nodes = hlp.flatten(nodes)
 
         for node in nodes:
             if node[1] != "NodeUndefined":
@@ -185,50 +147,12 @@ EgcNode*EgcNodeCreator::create(QLatin1String name)
         import cog
         import os
         import re
+        import sys
+        sys.path.append(BaseDirToSearch + "/../tools/")
+        import python_helper as hlp
 
-        def find_nodes_in_file(filename):
-            nodes = list()
-            regex = re.compile("[\s]*EGC_SET_EXPRESSION_TYPE\(([a-zA-Z0-9_-]+)[\s]*,[\s]*[a-zA-Z0-9]+::([a-zA-Z0-9]+)[\s]*\)[\s]*;[\s]*", re.MULTILINE)
-            with open(filename, "r") as f:
-                content = f.read();
-                result = regex.findall(content)
-                if result:
-                    nodes.append(result)
-                else:
-                    nodes = None
-                f.close()
-                return nodes
-            return None
-
-        def find_nodes_in_dir(base_path, sub_path):
-            nodes = list()
-            dir_complete = os.path.join(base_path, sub_path)
-            files = list()
-            for root, directories, filenames in os.walk(dir_complete):
-                for filename in filenames:
-                    files.append(os.path.join(root,filename))
-            for file in files:
-                if file.endswith(".h") or file.endswith(".H"):
-                    nodes_temp = find_nodes_in_file(file)
-                    if nodes_temp:
-                        nodes.append(nodes_temp)
-            if not nodes:
-                cog.error("Seems you have changed the macro name \"EGC_SET_EXPRESSION_TYPE\" please change the above regex also to function again!")
-            return nodes
-
-        def flatten(param):
-            if not param:
-                return []
-            elif type(param) is not list:
-                return [param]
-            else:
-                first_el = flatten(param[0])
-                el = flatten(param[1:])
-                first_el.extend(el)
-                return first_el
-
-        nodes = find_nodes_in_dir(BaseDirToSearch, "structural")
-        nodes = flatten(nodes)
+        nodes = hlp.find_nodes_in_dir(BaseDirToSearch, "structural")
+        nodes = hlp.flatten(nodes)
 
         for node in nodes:
             if node[1] != "NodeUndefined":
---
+++ egCAS/tools/python_helper.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python -e
+
+import os
+import re
+
+def find_nodes_in_file(filename):
+    nodes = list()
+    regex = re.compile("[\s]*EGC_SET_EXPRESSION_TYPE\(([a-zA-Z0-9_-]+)[\s]*,[\s]*[a-zA-Z0-9]+::([a-zA-Z0-9]+)[\s]*\)[\s]*;[\s]*", re.MULTILINE)
+    with open(filename, "r") as f:
+        content = f.read();
+        result = regex.findall(content)
+        if result:
+            nodes.append(result)
+        else:
+            nodes = None
+        f.close()
+        return nodes
+    return None
+
+def find_nodes_in_dir(base_path, sub_path):
+    nodes = list()
+    dir_complete = os.path.join(base_path, sub_path)
+    files = list()
+    for root, directories, filenames in os.walk(dir_complete):
+        for filename in filenames:
+            files.append(os.path.join(root,filename))
+    for file in files:
+        if file.endswith(".h") or file.endswith(".H"):
+            nodes_temp = find_nodes_in_file(file)
+            if nodes_temp:
+                nodes.append(nodes_temp)
+    if not nodes:
+        cog.error("Seems you have changed the macro name \"EGC_SET_EXPRESSION_TYPE\" please change the above regex also to function again!")
+    return nodes
+
+def flatten(param):
+    if not param:
+        return []
+    elif type(param) is not list:
+        return [param]
+    else:
+        first_el = flatten(param[0])
+        el = flatten(param[1:])
+        first_el.extend(el)
+        return first_el
+
