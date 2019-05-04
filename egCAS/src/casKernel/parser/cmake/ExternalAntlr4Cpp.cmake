deb0ad333c3ce18d5947c396ceb43dac5f07625c
jo <maier_jo@gmx.de>
2019-05-04 18:47:22 +0200

    :internal: fix windows build

--- egCAS/src/casKernel/parser/cmake/ExternalAntlr4Cpp.cmake
+++ egCAS/src/casKernel/parser/cmake/ExternalAntlr4Cpp.cmake
@@ -118,7 +118,7 @@ ExternalProject_Add_Step(
     DEPENDS antlr4_runtime
     BYPRODUCTS ${ANTLR4_STATIC_LIBRARIES}
     EXCLUDE_FROM_MAIN 1
-    WORKING_DIRECTORY ${ANTLR4_ROOT})
+    WORKING_DIRECTORY ${ANTLR4_ROOT}/runtime/Cpp)
 ExternalProject_Add_StepTargets(antlr4_runtime build_static)
 
 add_library(antlr4_static STATIC IMPORTED)
@@ -135,7 +135,7 @@ ExternalProject_Add_Step(
     DEPENDS antlr4_runtime
     BYPRODUCTS ${ANTLR4_SHARED_LIBRARIES} ${ANTLR4_RUNTIME_LIBRARIES}
     EXCLUDE_FROM_MAIN 1
-    WORKING_DIRECTORY ${ANTLR4_ROOT})
+    WORKING_DIRECTORY ${ANTLR4_ROOT}/runtime/Cpp)
 ExternalProject_Add_StepTargets(antlr4_runtime build_shared)
 
 add_library(antlr4_shared SHARED IMPORTED)
